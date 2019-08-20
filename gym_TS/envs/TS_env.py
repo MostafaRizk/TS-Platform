b663b5d27faf5ce2769cca48fac705adfa7a5989
Mostafa Rizk <mostafa.k.rizk@gmail.com>
2019-08-20 17:11:53 +1000

    Re-designed track

--- gym_TS/envs/TS_env.py
+++ gym_TS/envs/TS_env.py
@@ -20,24 +20,32 @@ class TSEnv(gym.Env):
         Initialise position, speed, gravity and velocity. Also defines action space and observation space. Creates seed?
         :param goal_velocity:
         '''
-        self.min_position = -1.2
-        self.max_position = 0.6
+        self.min_position = 0
+        self.max_position = 10
         self.max_speed = 0.07
-        self.goal_position = 0.5
+        self.goal_position = 8.5
         self.goal_velocity = goal_velocity
+        self.offset = 5
 
         self.force=0.001
         self.gravity=0.0025
+
+        self.slope_start = 2.5
+        self.slope_end = 7.5
+        self.slope_angle = 10
 
         self.low = np.array([self.min_position, -self.max_speed])
         self.high = np.array([self.max_position, self.max_speed])
 
         self.viewer = None
 
+        #TODO: Modify to have relevant actions and observations
         self.action_space = spaces.Discrete(3)
         self.observation_space = spaces.Box(self.low, self.high, dtype=np.float32)
 
         self.seed()
+
+        #TODO: Initialise neural network
 
     def seed(self, seed=None):
         '''
@@ -59,6 +67,7 @@ class TSEnv(gym.Env):
         assert self.action_space.contains(action), "%r (%s) invalid" % (action, type(action))
 
         position, velocity = self.state
+        '''
         velocity += (action-1)*self.force + math.cos(3*position)*(-self.gravity)
         velocity = np.clip(velocity, -self.max_speed, self.max_speed)
         position += velocity
@@ -66,6 +75,9 @@ class TSEnv(gym.Env):
 
         if (position==self.min_position and velocity<0):
             velocity = 0
+        '''
+
+        #TODO: Plug observations into NN
 
         done = bool(position >= self.goal_position and velocity >= self.goal_velocity)
         reward = -1.0
@@ -75,21 +87,39 @@ class TSEnv(gym.Env):
 
     def reset(self):
         '''
-        Sets a new random location between -0.6 and -0.4 and sets velocity to 0
+        Sets a new random location between 0 and 1 and sets velocity to 0
         :return:
         '''
-
-        self.state = np.array([self.np_random.uniform(low=-0.6, high=-0.4), 0])
+        #TODO: Redefine state to be (position, want_res, has_res, behav) ?
+        self.state = np.array([self.np_random.uniform(low=0, high=1), 0])
         return np.array(self.state)
+
+    def height_map(self, x):
+        '''
+        Maps an x coordinate on the track to the height of the track at that point
+        :param x:
+        :return:
+        '''
+        if x < self.slope_start:
+            return self.slope_start * math.tan(math.radians(self.slope_angle))
+        elif x < self.slope_end:
+            return x * math.tan(math.radians(self.slope_angle))
+        else:
+            return self.slope_end * math.tan(math.radians(self.slope_angle))
 
     def _height(self, xs):
         '''
-        Returns...height?
+        Returns height according to the function representing the track
         :param xs:
         :return:
         '''
-        #return np.sin(3 * xs)*.45+.55
-        return (1 / (1+np.exp(-10*xs)))*0.75+0.1
+        if not isinstance(xs,np.ndarray):
+            return self.height_map(xs)
+        else:
+            ys = np.copy(xs)
+            for i in range(len(xs)):
+                ys[i] = self.height_map(xs[i])
+            return ys
 
     def render(self, mode='human'):
         '''
@@ -109,11 +139,13 @@ class TSEnv(gym.Env):
 
 
         if self.viewer is None:
-            #Create track
+            #Define the geometry of the track
             from gym.envs.classic_control import rendering
             self.viewer = rendering.Viewer(screen_width, screen_height)
             xs = np.linspace(self.min_position, self.max_position, 100)
             ys = self._height(xs)
+            print(xs)
+            print(ys)
             xys = list(zip((xs-self.min_position)*scale, ys*scale))
 
             self.track = rendering.make_polyline(xys)
@@ -144,6 +176,7 @@ class TSEnv(gym.Env):
             #Create resource
             l, r, t, b = (self.goal_position-self.min_position)*scale -reswidth / 2, (self.goal_position-self.min_position)*scale + reswidth / 2, self._height(self.goal_position)*scale + resheight, self._height(self.goal_position)*scale
             resource = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
+            #TODO: Allow movement of resource
             #resource.add_attr(rendering.Transform(translation=(0, clearance)))
             #self.cartrans = rendering.Transform()
             #car.add_attr(self.cartrans)
@@ -154,7 +187,7 @@ class TSEnv(gym.Env):
         #Set position of car
         pos = self.state[0]
         self.cartrans.set_translation((pos-self.min_position)*scale, self._height(pos)*scale)
-        self.cartrans.set_rotation(math.cos(3 * pos))
+        #self.cartrans.set_rotation(math.cos(3 * pos))
 
         return self.viewer.render(return_rgb_array = mode=='rgb_array')
 
