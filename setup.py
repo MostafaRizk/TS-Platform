c302820a642abc005b161fdbacfd32fe16c61fa7
Julian Garcia <julian.garcia@monash.edu>
2020-03-03 12:11:04 +1100

    Clean UP

--- setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-
-setup(name='gym_TS',
-      version='0.0.1',
-      install_requires=['gym', 'numpy', 'tinynet', 'cma', 'scipy', 'matplotlib', 'pandas', 'scikit-learn']#, 'keras', 'tensorflow']
-)
