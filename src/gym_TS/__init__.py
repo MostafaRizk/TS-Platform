bbef456fb9acc1e021ffd1cd91bd2ee6a8d963c1
Mostafa <mostafa.k.rizk@gmail.com>
2020-04-20 12:27:54 +1000

    Changed directory system to support building singularity container

--- src/gym_TS/__init__.py
+++
@@ -1,6 +0,0 @@
-from gym.envs.registration import register
-
-register(
-    id='TS-v1',
-    entry_point='src.gym_TS.envs:SlopeEnvGym',
-)
--- src/gym_TS/envs/__init__.py
+++
@@ -1,1 +0,0 @@
-from src.gym_TS.envs.slope_env_gym import SlopeEnvGym
