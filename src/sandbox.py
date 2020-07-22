5d8a1ea6940c50ba28a057fac63049f9f6fa64f9
Mostafa Rizk <mostafa.k.rizk@gmail.com>
2020-07-22 11:48:08 +1000

    Prepared new experiments

--- src/sandbox.py
+++ src/sandbox.py
@@ -1,11 +1,21 @@
 import argparse
+
+from agents.hardcoded.collector import HardcodedCollectorAgent
+from agents.hardcoded.dropper import HardcodedDropperAgent
+from agents.hardcoded.generalist import HardcodedGeneralistAgent
 from fitness import FitnessCalculator
 from learning.rwg import RWGLearner
+import numpy as np
 
-parser = argparse.ArgumentParser(description='Run an experiment')
-parser.add_argument('--parameters', action="store", dest="parameter_filename")
-parameter_filename = parser.parse_args().parameter_filename
-
+parameter_filename = "default_parameters.json"
 fitness_calculator = FitnessCalculator(parameter_filename)
-learner = RWGLearner(fitness_calculator)
-genome, fitness = learner.learn()
+
+agent_1 = HardcodedGeneralistAgent()
+agent_2 = HardcodedGeneralistAgent()
+results = fitness_calculator.calculate_fitness(agent_1, agent_2)
+fitness_1_list = results["fitness_1_list"]
+fitness_2_list = results["fitness_2_list"]
+specialisation_list = results["specialisation_list"]
+zipped_list = zip(fitness_1_list, fitness_2_list)
+print(f"{np.mean([fitness_1 + fitness_2 for (fitness_1, fitness_2) in zipped_list])}")
+
