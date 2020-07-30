dd7bc48f10712a96a7363578ec84fea72ec10f30
Mostafa Rizk <mostafa.k.rizk@gmail.com>
2020-07-30 14:56:41 +1000

    Updated benchmarking report

--- src/sandbox.py
+++ src/sandbox.py
@@ -8,17 +8,17 @@ from learning.rwg import RWGLearner
 import numpy as np
 from agents.nn_agent_lean import NNAgent
 
-parameter_filename = "no_battery.json"
+parameter_filename = "sliding_1.json"
+#model_name = "rwg_homogeneous_team_nn_1_2_3_1_0_8_4_1_3_7_1_3.0_0.2_2_1000_500_5_rnn_False_0_0_linear_100000_normal_0_1_39082.27999999997.npy"
+model_name = "rwg_homogeneous_team_nn_1_2_3_1_1_8_4_1_3_7_1_3.0_0.2_2_1000_500_5_rnn_False_0_0_linear_100000_normal_0_1_39879.19999999997.npy"
 fitness_calculator = FitnessCalculator(parameter_filename)
 
-agent_1 = HardcodedDropperAgent()
-agent_2 = HardcodedCollectorAgent()
-results = fitness_calculator.calculate_fitness(agent_1, agent_2)
+genome = NNAgent.load_model_from_file(model_name)
+agent_1 = NNAgent(fitness_calculator.get_observation_size(), fitness_calculator.get_action_size(), parameter_filename, genome)
+agent_2 = NNAgent(fitness_calculator.get_observation_size(), fitness_calculator.get_action_size(), parameter_filename, genome)
+results = fitness_calculator.calculate_fitness(agent_1, agent_2, render=True, time_delay=10) # , render=True, time_delay=0.1
 fitness_1_list = results["fitness_1_list"]
 fitness_2_list = results["fitness_2_list"]
 specialisation_list = results["specialisation_list"]
 zipped_list = zip(fitness_1_list, fitness_2_list)
 print(f"{np.mean([fitness_1 + fitness_2 for (fitness_1, fitness_2) in zipped_list])}")
-
-#114.4
-#158.4
--- src/scripts/benchmark_plot_generator.py
+++ src/scripts/benchmark_plot_generator.py
@@ -48,7 +48,41 @@ import os
                ("rnn_no-bias_2HL_4HU_sigmoid", "result_files/rnn_no-bias_2HL_4HU_sigmoid.csv"),"""
 
 
-experiments = [("no_battery", "result_files/no_battery.csv")
+experiments = [("ffnn_bias_0HL", "result_files/ffnn_bias_0HL.csv"),
+               ("ffnn_no-bias_0HL", "result_files/ffnn_no-bias_0HL.csv"),
+               ("rnn_no-bias_0HL", "result_files/rnn_no-bias_0HL.csv"),
+               ("sliding_0", "result_files/sliding_0.csv"),
+               ("sliding_1", "result_files/sliding_1.csv"),
+               ("sliding_2", "result_files/sliding_2.csv"),
+               ("sliding_3", "result_files/sliding_3.csv"),
+               ("sliding_5", "result_files/sliding_5.csv"),
+               ("sliding_6", "result_files/sliding_6.csv"),
+               ("single_agent", "result_files/single_agent.csv"),
+               ("no_battery", "result_files/no_battery.csv"),
+               ("rnn_no-bias_0HL_tanh", "result_files/rnn_no-bias_0HL_tanh.csv"),
+               ("rnn_no-bias_1HL_4HU_tanh", "result_files/rnn_no-bias_1HL_4HU_tanh.csv"),
+               ("rnn_no-bias_2HL_4HU_tanh", "result_files/rnn_no-bias_2HL_4HU_tanh.csv"),
+               ("rnn_no-bias_0HL_relu", "result_files/rnn_no-bias_0HL_relu.csv"),
+               ("rnn_no-bias_1HL_4HU_relu", "result_files/rnn_no-bias_1HL_4HU_relu.csv"),
+               ("rnn_no-bias_2HL_4HU_relu", "result_files/rnn_no-bias_2HL_4HU_relu.csv"),
+               ("rnn_no-bias_0HL_sigmoid", "result_files/rnn_no-bias_0HL_sigmoid.csv"),
+               ("rnn_bias_0HL_tanh", "result_files/rnn_bias_0HL_tanh.csv"),
+               ("rnn_bias_1HL_4HU_tanh", "result_files/rnn_bias_1HL_4HU_tanh.csv"),
+               ("rnn_bias_2HL_4HU_tanh", "result_files/rnn_bias_2HL_4HU_tanh.csv"),
+               ("rnn_bias_0HL_relu", "result_files/rnn_bias_0HL_relu.csv"),
+               ("rnn_bias_1HL_4HU_relu", "result_files/rnn_bias_1HL_4HU_relu.csv"),
+               ("rnn_bias_2HL_4HU_relu", "result_files/rnn_bias_2HL_4HU_relu.csv"),
+               ("rnn_bias_0HL_sigmoid", "result_files/rnn_bias_0HL_sigmoid.csv"),
+               ("rnn_bias_1HL_4HU_sigmoid", "result_files/rnn_bias_1HL_4HU_sigmoid.csv"),
+               ("rnn_bias_2HL_4HU_sigmoid", "result_files/rnn_bias_2HL_4HU_sigmoid.csv"),
+               ("ffnn_no-bias_0HL_tanh", "result_files/ffnn_no-bias_0HL_tanh.csv"),
+               ("ffnn_no-bias_1HL_4HU_tanh", "result_files/ffnn_no-bias_1HL_4HU_tanh.csv"),
+               ("ffnn_no-bias_2HL_4HU_tanh", "result_files/ffnn_no-bias_2HL_4HU_tanh.csv"),
+               ("ffnn_bias_0HL_tanh", "result_files/ffnn_bias_0HL_tanh.csv"),
+               ("ffnn_bias_1HL_4HU_tanh", "result_files/ffnn_bias_1HL_4HU_tanh.csv"),
+               ("ffnn_bias_2HL_4HU_tanh", "result_files/ffnn_bias_2HL_4HU_tanh.csv"),
+               ("rnn_no-bias_1HL_4HU_sigmoid", "result_files/rnn_no-bias_1HL_4HU_sigmoid.csv"),
+               ("rnn_no-bias_2HL_4HU_sigmoid", "result_files/rnn_no-bias_2HL_4HU_sigmoid.csv")
                ]
 
 original_dir = os.getcwd()
@@ -60,6 +94,10 @@ for experiment in experiments:
     plotter = BenchmarkPlotter(experiment_name, experiment_filename)
     #plotter.save_all_sample_stats(N_bins=[-5000, 0, 5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000],
     #                              mean_lim=(-3000, 50000), var_lim=(0, 15000), dist_lim=(1, 100000))
+    #plotter.save_all_sample_stats(
+    #    N_bins=[0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50],
+    #                              mean_lim=(0, 50), var_lim=(0, 15), dist_lim=(1, 100))
+
     plotter.save_all_sample_stats(
-        N_bins=[0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50],
-                                  mean_lim=(0, 50), var_lim=(0, 15), dist_lim=(1, 100))
+        N_bins=[-5000, 0, 5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000],
+                                  mean_lim=(-3000, 50000), var_lim=(0, 15000), dist_lim=(10**-1, 100000))
