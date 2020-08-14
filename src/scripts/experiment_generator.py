dceadf5fdb2ce5d22a7dfdbdcc7397c46ab94eee
Mostafa Rizk <mostafa.k.rizk@gmail.com>
2020-08-14 14:54:41 +1000

    Prepared heterogeneous experiments

--- src/scripts/experiment_generator.py
+++ src/scripts/experiment_generator.py
@@ -5,7 +5,10 @@ import numpy as np
 
 from learning.cma import CMALearner
 from learning.learner_parent import Learner
+from glob import glob
 
+# Generate CMA experiments
+'''
 core_parameter_filename = "../default_parameters.json"
 parameter_dictionary = json.loads(open(core_parameter_filename).read())
 num_experiments = 30
@@ -26,3 +29,20 @@ for i in range(num_experiments):
     g.write(f"python3 sandbox.py --parameters {filename}\n")
 
 g.close()
+'''
+
+#for every json file except lazy_generalist and default_parameters
+json_files = glob(f'../*nn*.json')
+
+for parameter_filename in json_files:
+    # load parameter dictionary
+    parameter_dictionary = json.loads(open(parameter_filename).read())
+
+    # change to heterogneous
+    parameter_dictionary["general"]["team_type"] = "heterogneous"
+
+    # write to file
+    f = open(parameter_filename, "w")
+    dictionary_string = json.dumps(parameter_dictionary, indent=4)
+    f.write(dictionary_string)
+    f.close()
