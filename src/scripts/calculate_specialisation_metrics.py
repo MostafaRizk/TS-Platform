Fixed error in specialisation calculation

src/scripts/calculate_specialisation_metrics.py
@@ -4,9 +4,10 @@ import pandas as pd
 
 from fitness import FitnessCalculator
 from agents.nn_agent_lean import NNAgent
+from scipy.stats import multivariate_normal
 
 rwg_parameter_file = "rnn_no-bias_1HL_4HU_tanh.json"
-rwg_genomes_file = "../sammon_testing_rwg.csv"
+rwg_genomes_file = "rwg_with_spec.csv"
 N_episodes = 20
 
 evolved_genomes_parameters_directory = "/Users/mostafa/Documents/Code/PhD/TS-Platform/results/2020_11_24_magic_plot_combined_new_seed/experiments"
@@ -14,38 +15,13 @@ evolved_genomes_results_directory = "/Users/mostafa/Documents/Code/PhD/TS-Platfo
 results_file = os.path.join(evolved_genomes_results_directory,"results_final.csv")
 
 # Create data file
-specialisation_file = "specialisation_final_practice.csv"  # os.path.join(evolved_genomes_results_directory,"specialisation_final.csv")
-f = open(specialisation_file, "a")
-#f.write(f"Model Name,R_coop,R_coop_eff,R_spec,R_coop x P,R_coop_eff x P,R_spec x P,Model Directory\n")
-f.write("\n")
+specialisation_file = "specialisation_equal_samples.csv"  # os.path.join(evolved_genomes_results_directory,"specialisation_final.csv")
+f = open(specialisation_file, "w")
+f.write(f"Model Name,Team Fitness,R_coop,R_coop_eff,R_spec,R_coop x P,R_coop_eff x P,R_spec x P,Model Directory\n")
+#f.write("\n")
 
-'''
-# Get rwg genome data
-g = open(rwg_genomes_file, "r")
-rwg_data = g.read().strip().split("\n")
-g.close()
-rwg_indices = [i for i in range(len(rwg_data))]
-rwg_fitness_calculator = FitnessCalculator(rwg_parameter_file)
-
-
-for index in rwg_indices:
-    row = rwg_data[index]
-    full_genome = [float(element) for element in row.split(",")[0:-N_episodes]]
-    mid = int(len(full_genome) / 2)
-    genome_part_1 = full_genome[0:mid]
-    genome_part_2 = full_genome[mid:]
-    agent_1 = NNAgent(rwg_fitness_calculator.get_observation_size(), rwg_fitness_calculator.get_action_size(),
-                      rwg_parameter_file, genome_part_1)
-    agent_2 = NNAgent(rwg_fitness_calculator.get_observation_size(), rwg_fitness_calculator.get_action_size(),
-                      rwg_parameter_file, genome_part_2)
-    results = rwg_fitness_calculator.calculate_fitness(agent_list=[agent_1, agent_2], render=False, time_delay=0,
-                                                   measure_specialisation=True, logging=False, logfilename=None,
-                                                   render_mode="human")
-    specialisation_measures = np.mean(np.array(results["specialisation_list"]), axis=0)
-    f.write(",".join([str(index)] + [str(measure) for measure in specialisation_measures] + [rwg_genomes_file]) + "\n")
-'''
 ''''''
-for generation in [100, 200, 300, 400, 500, 600, 700, 800, 900]:
+for generation in ["final"]:
     results_file = os.path.join(evolved_genomes_results_directory,f"results_{generation}.csv")
 
     # Load all evolved genomes into pandas dataframe
@@ -74,8 +50,12 @@ for generation in [100, 200, 300, 400, 500, 600, 700, 800, 900]:
             results = fitness_calculator.calculate_fitness(agent_list=[agent_1, agent_2], render=False, time_delay=0,
                                                            measure_specialisation=True, logging=False, logfilename=None,
                                                            render_mode="human")
+
+            fitness_matrix = results["fitness_matrix"]
+            new_fitness = np.mean([fitness_matrix[0][episode] + fitness_matrix[1][episode] for episode in range(len(fitness_matrix[0]))])
             specialisation_measures = np.mean(np.array(results["specialisation_list"]), axis=0)
-            f.write(",".join([row["model_name"]] + [str(measure) for measure in specialisation_measures] + [evolved_genomes_results_directory]) + "\n")
+
+            f.write(",".join([row["model_name"]] + [str(new_fitness)] + [str(measure) for measure in specialisation_measures] + [evolved_genomes_results_directory]) + "\n")
 
 
         ''''''
@@ -87,16 +67,41 @@ for generation in [100, 200, 300, 400, 500, 600, 700, 800, 900]:
             parameter_filename = os.path.join(evolved_genomes_parameters_directory, "_".join(row["model_name"].split("_")[:-2]) + ".json")
             fitness_calculator = FitnessCalculator(parameter_filename)
 
-            genome_1 = np.load(model_file)
-            genome_2 = np.load(model_file)
+            mean_array = np.load(model_file)
+            variance = None
 
-            agent_1 = NNAgent(fitness_calculator.get_observation_size(), fitness_calculator.get_action_size(), parameter_filename, genome_1)
-            agent_2 = NNAgent(fitness_calculator.get_observation_size(), fitness_calculator.get_action_size(), parameter_filename, genome_2)
+            log_file = os.path.join(evolved_genomes_results_directory, "_".join(row["model_name"].split("_")[:-1]) + ".log")
+            g = open(log_file, "r")
+            log_data = g.read().strip().split("\n")
+            g.close()
+
+            # If last line starts with Best, get line before
+            flag = True
+            i = -1
+            while flag:
+                if log_data[i].startswith("Best"):
+                    variance = float(log_data[i - 1].strip().split()[4]) / 100
+                    flag = False
+                else:
+                    i -= 1
+
+            seed = row["seed"]
+
+            random_variable = multivariate_normal(mean=mean_array, cov=np.identity(len(mean_array)) * variance)
+            team = random_variable.rvs(2, seed)
+
+            agent_1 = NNAgent(fitness_calculator.get_observation_size(), fitness_calculator.get_action_size(),
+                              parameter_filename, team[0])
+            agent_2 = NNAgent(fitness_calculator.get_observation_size(), fitness_calculator.get_action_size(),
+                              parameter_filename, team[1])
             results = fitness_calculator.calculate_fitness(agent_list=[agent_1, agent_2], render=False, time_delay=0,
                                                            measure_specialisation=True, logging=False, logfilename=None,
                                                            render_mode="human")
+            fitness_matrix = results["fitness_matrix"]
+            new_fitness = np.mean([fitness_matrix[0][episode] + fitness_matrix[1][episode] for episode in range(len(fitness_matrix[0]))])
             specialisation_measures = np.mean(np.array(results["specialisation_list"]), axis=0)
-            f.write(",".join([row["model_name"]] + [str(measure) for measure in specialisation_measures] + [evolved_genomes_results_directory]) + "\n")
+
+            f.write(",".join([row["model_name"]] + [str(new_fitness)] + [str(measure) for measure in specialisation_measures] + [evolved_genomes_results_directory]) + "\n")
 
 
 f.close()

src/scripts/dimensionality_reduction.py
@@ -8,19 +8,32 @@ from mpl_toolkits import mplot3d
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 
-rwg_genomes_file = "../all_genomes_rwg_heterogeneous_team_nn_slope_1_2_4_1_4_8_4_1_3_7_1_3.0_0.2_2_1000_500_20_rnn_False_1_4_tanh_2000_lhs_-6_6_.csv"
-f = open(rwg_genomes_file, "r")
-rwg_data = f.read().strip().split("\n")
+class DimensionalityReducer:
+    def __init__(self):
+        pass
+
+    def generate_plot(self):
+        pass
+
+
+#rwg_genomes_files = ["rwg_with_spec.csv"] #+ [f"extra_rwg_{i}.csv" for i in range(6,7)]
+#rwg_genomes_files = [f"extra_rwg_{i}.csv" for i in range(3,4)]
+rwg_genomes_files = ["rwg_lhs_5.csv"]
+
+rwg_data = []
+for rwg_genomes_file in rwg_genomes_files:
+    f = open(rwg_genomes_file, "r")
+    rwg_data += f.read().strip().split("\n")
 N_episodes = 20
 min_score = -20000
 max_score = 200000
 matrix = []
 scores = []
-rwg_indices = [i for i in range(500)] + [i for i in range(-500,0)]
+rwg_indices = [i for i in range(60)] + [i for i in range(-60,0)]
 
 evolved_genomes_directory = "/Users/mostafa/Documents/Code/PhD/TS-Platform/results/2020_11_24_magic_plot_combined_new_seed/results"
 
-specialisation_file = "specialisation_final_practice.csv"
+specialisation_file = "specialisation_equal_samples.csv"
 specialisation_data = pd.read_csv(specialisation_file)
 spec_score_keys = ["R_coop", "R_coop_eff", "R_spec", "R_coop x P", "R_coop_eff x P", "R_spec x P"]
 spec_scores = {}
@@ -54,19 +67,6 @@ for index in rwg_indices:
     matrix += [genome]
     scores += [mean_score]
 
-    '''
-    # Find row in spec dataframe with matching index and filename
-    matching_rows = specialisation_data.loc[ (specialisation_data['Model Name'] == str(index)) & (specialisation_data['Model Directory'] == rwg_genomes_file) ]
-
-    if len(matching_rows) != 1:
-        raise RuntimeError("Matching entries in the specialisation file is not 1")
-
-    # Store spec scores from the dataframe in the dictionary
-    for index, row in matching_rows.iterrows():
-        for key in spec_scores.keys():
-            spec_scores[key] += [row[key]]
-    '''
-
     num_rwg += 1
 
 # Get evolved data
@@ -83,8 +83,6 @@ for generation in ["final"]:
             genome = np.load(model_file)
             # Add genome to matrix
             matrix += [genome]
-            # Add fitness to scores
-            scores += [row["fitness"]]
 
             num_team += 1
 
@@ -96,8 +94,7 @@ for generation in ["final"]:
             genome = np.load(model_file)
             # Add genome to matrix
             matrix += [np.append(genome,genome)]
-            # Add fitness to scores
-            scores += [row["fitness"]*2]
+
             num_ind += 1
 
         if row["num_agents"] == 2:
@@ -109,24 +106,32 @@ for generation in ["final"]:
 
             # Store spec scores from the dataframe in the dictionary
             for index2, row2 in matching_rows.iterrows():
+                # Add fitness to scores
+                scores += [row2["Team Fitness"]]
+
+                # Add specialisation scores
                 for key in spec_scores.keys():
                     spec_scores[key] += [row2[key]]
 
 scores = np.array(scores)
 
-# Do dimensionality reduction using sammon mapping
-# By default, sammon returns a 2-dim array and the error E
+# Do dimensionality reduction using PCA
 '''
-[new_data, error] = sammon.sammon(np.array(matrix), n=2)
-x = new_data[:, 0]
-y = new_data[:, 1]
+pca = PCA(n_components=200)
+pca_result = pca.fit_transform(np.array(matrix))
+print(np.sum(pca.explained_variance_ratio_))
+x = pca_result[:,0]
+y = pca_result[:,1]
 '''
 
 # Do dimensionality reduction using t-SNE
+''''''
 tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=1000)
 tsne_results = tsne.fit_transform(np.array(matrix))
+#tsne_results = tsne.fit_transform(pca_result)
 x = tsne_results[:,0]
 y = tsne_results[:,1]
+
 
 z = scores
 
@@ -141,7 +146,6 @@ for i in range(1,7):
     cm = plt.cm.get_cmap('RdYlGn')
     key = spec_score_keys[i-1]
 
-    #sc = plt.scatter(x, y, c=scores, vmin=min_score, vmax=max_score, cmap=cm)
     p = ax.scatter3D(x[rwg_start_index : team_start_index], y[rwg_start_index : team_start_index], z[rwg_start_index : team_start_index], c=spec_scores[key][rwg_start_index : team_start_index], vmin=0, vmax=1, cmap=cm, label="Rwg genomes")
     p = ax.scatter3D(x[team_start_index : ind_start_index], y[team_start_index : ind_start_index], z[team_start_index : ind_start_index], c=spec_scores[key][team_start_index : ind_start_index], vmin=0, vmax=1, cmap=cm, label="Team Genomes")
     p = ax.scatter3D(x[ind_start_index:], y[ind_start_index:], z[ind_start_index:], c=spec_scores[key][ind_start_index:], vmin=0, vmax=1, cmap=cm, label="Individual Genomes")
@@ -151,7 +155,7 @@ for i in range(1,7):
 #ax.colorbar()
 plt.suptitle("3D Mapping of Fitness Landscape")
 #fig.legend(loc="lower left")
-#plt.savefig("t-SNE_v3.png")
-plt.show()
+plt.savefig(f"tsne_new_spec_lhs_5_60_samples.png")
+#plt.show()
 
 