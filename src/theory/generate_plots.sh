527da4cf592b4b898bcb92d62e3955b840db661d
Mostafa Rizk <mostafa.k.rizk@gmail.com>
2021-04-07 10:51:08 +1000

    Modified plotting bash script

--- src/theory/generate_plots.sh
+++ src/theory/generate_plots.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-python3 evolutionary_dynamics.py --parameters /Users/mostafa/Documents/Code/PhD/TS-Platform/results/2021_04_07_evolutionary_dynamics/mostly_spec_slope_4.json
-python3 evolutionary_dynamics.py --parameters /Users/mostafa/Documents/Code/PhD/TS-Platform/results/2021_04_07_evolutionary_dynamics/mostly_spec_slope_0.json
-python3 evolutionary_dynamics.py --parameters /Users/mostafa/Documents/Code/PhD/TS-Platform/results/2021_04_07_evolutionary_dynamics/mostly_novice_slope_0.json
-python3 evolutionary_dynamics.py --parameters /Users/mostafa/Documents/Code/PhD/TS-Platform/results/2021_04_07_evolutionary_dynamics/mostly_novice_slope_4.json
+#python3 evolutionary_dynamics.py --parameters /Users/mostafa/Documents/Code/PhD/TS-Platform/results/2021_04_07_evolutionary_dynamics/mostly_spec_slope_4.json
+#python3 evolutionary_dynamics.py --parameters /Users/mostafa/Documents/Code/PhD/TS-Platform/results/2021_04_07_evolutionary_dynamics/mostly_spec_slope_0.json
+#python3 evolutionary_dynamics.py --parameters /Users/mostafa/Documents/Code/PhD/TS-Platform/results/2021_04_07_evolutionary_dynamics/mostly_novice_slope_0.json
+#python3 evolutionary_dynamics.py --parameters /Users/mostafa/Documents/Code/PhD/TS-Platform/results/2021_04_07_evolutionary_dynamics/mostly_novice_slope_4.json
+python3 evolutionary_dynamics.py --parameters /Users/mostafa/Documents/Code/PhD/TS-Platform/results/2021_04_07_evolutionary_dynamics/even_split_slope_4.json
+python3 evolutionary_dynamics.py --parameters /Users/mostafa/Documents/Code/PhD/TS-Platform/results/2021_04_07_evolutionary_dynamics/even_split_slope_0.json
