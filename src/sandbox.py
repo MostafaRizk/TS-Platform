3b88220e1525826fa2e76522702642a8a8294785
Mostafa Rizk <mostafa.k.rizk@gmail.com>
2021-12-10 11:41:31 +1100

    Preparing for rwg plotting

--- src/sandbox.py
+++ src/sandbox.py
@@ -4,7 +4,7 @@ from scripts.benchmarking import plot_envs_vs_NN_arch
 
 
 def generate_plots(genome_directory, metric=None):
-    biases = [False]  # , True]
+    biases = [False, True]
     if metric is None:
         # metrics = ["R_coop", "R_coop_eff", "R_spec"]
         metrics = ["R_coop"]
@@ -15,10 +15,10 @@ def generate_plots(genome_directory, metric=None):
         for bias in biases:
             for m in metrics:
                 # Proper code
-                # plot_envs_vs_NN_arch(genome_directory, bias, N_bins=list(range(-2000, 45000, 10000)), mean_lim=(-2000, 45000), var_lim=(0, 15000), dist_lim=(10 ** -1, 10000), spec_metric_key=m, num_samples=10000, num_episodes=20, env="slope", learning_type=learning_type)
+                plot_envs_vs_NN_arch(genome_directory, bias, N_bins=list(range(-2000, 45000, 10000)), mean_lim=(-2000, 45000), var_lim=(0, 15000), dist_lim=(10 ** -1, 20000), spec_metric_key=m, num_samples=10000, num_episodes=20, env="slope", learning_type=learning_type)
 
                 # Dummy code for testing
-                plot_envs_vs_NN_arch(genome_directory, bias, N_bins=list(range(-2000, 45000, 10000)), mean_lim=(-2000, 45000), var_lim=(0, 15000), dist_lim=(10 ** -1, 20000), spec_metric_key=m, num_samples=50, num_episodes=5, env="slope", learning_type=learning_type)
+                # plot_envs_vs_NN_arch(genome_directory, bias, N_bins=list(range(-2000, 45000, 10000)), mean_lim=(-2000, 45000), var_lim=(0, 15000), dist_lim=(10 ** -1, 20000), spec_metric_key=m, num_samples=50, num_episodes=5, env="slope", learning_type=learning_type)
 
 parser = argparse.ArgumentParser(description='Generate RWG Analysis Plots')
 parser.add_argument('--genome_directory', action="store", dest="genome_directory")
