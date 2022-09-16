1089ae8d1b22f90aa79b4e839b3539beef594a53
Mostafa Rizk <mostafa.k.rizk@gmail.com>
2022-09-16 11:54:42 +1000

    restructuring

--- src/theory/generate_plots_aamas.sh
+++ src/theory/generate_plots_aamas.sh
@@ -3,8 +3,8 @@
 folder_name=/Users/mostafa/Documents/Code/PhD/TS-Platform/results/2022_09_14_replicator_mutator
 
 # Plot simplex
-time python3 evolutionary_dynamics.py --parameters $folder_name/distribution_params_b_2_agents_alignment=0.9.json --function plot_simplex
-
+#time python3 evolutionary_dynamics.py --parameters $folder_name/distribution_params_b_2_agents_alignment=0.9.json --function plot_simplex
+time python3 evolutionary_dynamics.py --parameters $folder_name/distribution_params_b_2_agents_alignment=0.9_some_mutation.json --function plot_simplex
 
 
 
